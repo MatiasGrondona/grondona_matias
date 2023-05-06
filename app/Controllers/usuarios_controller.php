ecbd9bd8b789f4e50c9571e8c9c077914f1b6bec
Matias Grondona <matiasgrondona@outlook.com>
2023-05-06 00:43:19 -0300

    Se continuo con el back para registrar usuarios

--- app/Controllers/usuarios_controller.php
+++ app/Controllers/usuarios_controller.php
@@ -1,20 +1,12 @@
 <?php
 namespace App\Controllers;
+use App\Models\Usuarios_model;
 use CodeIgniter\Controller;
 
 class Usuarios_controller extends Controller {
 
     public function __construct() {
         helper(['form', 'url']);
-    }
-
-    public function index() 
-    {
-        echo "Hola mundo";
-    }
-
-    public function prueba(){
-        echo "Hola mundo";
     }
 
     public function create(){
@@ -24,5 +16,43 @@ class Usuarios_controller extends Controller {
         . view('back/usuario/nuevoUsuario') 
         . view('front/pie');
     }
+
+    public function formValidation() {
+             
+        $input = $this->validate([
+            'nombre'   => 'required|min_length[3]',
+            'apellido' => 'required|min_length[3]|max_length[25]',
+            'email'    => 'required|min_length[4]|max_length[100]|valid_email|is_unique[usuarios.email]',
+            'usuario'  => 'required|min_length[3]',
+            'pass'     => 'required|min_length[3]|max_length[10]'
+        ],
+        
+       );
+        $formModel = new Usuarios_model();
+     
+        if (!$input) {
+               $data['titulo']='Registro'; 
+                echo view('front/head_view',$data);
+                echo view('front/nav_view');
+                //echo view('back/usuario/registrarse', ['validation' => $this->validator]);
+                echo view('back/usuario/registro', ['validation' => $this->validator]);
+                echo view('front/footer_view');
+
+        } else {
+            $formModel->save([
+                'nombre' => $this->request->getVar('nombre'),
+                'apellido'=> $this->request->getVar('apellido'),
+                'usuario'=> $this->request->getVar('usuario'),
+                'email'=> $this->request->getVar('email'),
+                'pass' => password_hash($this->request->getVar('pass'), PASSWORD_DEFAULT)
+              //  password_hash() crea un nuevo hash de contraseña usando un algoritmo de hash de único sentido.
+            ]);  
+              //return $this->response->redirect(site_url(''));
+
+            // Flashdata funciona solo en redirigir la función en el controlador en la vista de carga.
+               session()->setFlashdata('success', 'Usuario registrado con exito');
+               return $this->response->redirect(site_url('/registro'));
+      
+        }
+    }
 }
-?>
--- app/Views/back/usuario/nuevoUsuario.php
+++ app/Views/back/usuario/nuevoUsuario.php
@@ -1,14 +1,70 @@
-<?php &validation
+<div>
+    <!--recuperamos datos con la función Flashdata para mostrarlos-->
+    <?php if (session()->getFlashdata('success')) {
+      echo "
+      <div class='mt-3 mb-3 ms-3 me-3 h4 text-center alert alert-success alert-dismissible'>
+      <button type='button' class='btn-close' data-bs-dismiss='alert'></button>" . session()->getFlashdata('success') . "
+  </div>";
+    } ?>
+</div>
+<!-- php $validación = \Config\Services::validación(); Esto carga automáticamente el archivo Config\Validation que contiene configuraciones para incluir múltiples conjuntos de reglas -->
+<?php $validation = \Config\Services::validation(); ?>
 <div class="container">
     <div class="card">
         <div class="card-body">
             <h1>Nuevo Usuario</h1>
-            <form method="post" action="">
+            <form method="post" action="<?php echo base_url('/enviar-form') ?>">
+                <!-- NOMBRE -->
                 <label for="nombre" class="form-label">Nombre</label>
-                <input name="nombre" type="text" class="form-control" value="">
-
-                <button class="btn-primary">Regsitrarme</button>
-                <button class="btn-danger"></button>
+                <input name="nombre" type="text" class="form-control" value="<?php echo set_value('nombre')?>"
+                    placeholder="Nombre">
+                <!-- Error -->
+                <?php if ($validation->getError('nombre')) { ?>
+                <div class='alert alert-danger mt-2'>
+                    <?= $error = $validation->getError('nombre'); ?>
+                </div>
+                <?php } ?>
+                <!-- APELLIDO -->
+                <label for="apellido" class="form-label">Apellido</label>
+                <input type="text" name="apellido" class="form-control" value="<?php echo set_value('apellido')?>"
+                    placeholder="Apellido">
+                <!-- Error -->
+                <?php if ($validation->getError('apellido')) { ?>
+                <div class='alert alert-danger mt-2'>
+                    <?= $error = $validation->getError('apellido'); ?>
+                </div>
+                <?php } ?>
+                <!-- EMAIL -->
+                <label for="email" class="form-label">Email</label>
+                <input name="email" type="email" class="form-control" value="<?php echo set_value('email')?>"
+                    placeholder="correo@algo.com">
+                <!-- Error -->
+                <?php if ($validation->getError('email')) { ?>
+                <div class='alert alert-danger mt-2'>
+                    <?= $error = $validation->getError('email'); ?>
+                </div>
+                <?php } ?>
+                <!-- USUARIO -->
+                <label for="usuario" class="form-label">Usuario</label>
+                <input type="text" name="usuario" value="<?php echo set_value('usuario')?>" class="form-control"
+                    placeholder="Usuario">
+                <!-- Error -->
+                <?php if ($validation->getError('usuario')) { ?>
+                <div class='alert alert-danger mt-2'>
+                    <?= $error = $validation->getError('usuario'); ?>
+                </div>
+                <?php } ?>
+                <!-- PASSWORD -->
+                <label for="pass" class="form-label">Password</label>
+                <input name="pass" type="password" class="form-control" placeholder="Contraseña (mínimo 5 caracteres)">
+                <!-- Error -->
+                <?php if ($validation->getError('pass')) { ?>
+                <div class='alert alert-danger mt-2'>
+                    <?= $error = $validation->getError('pass'); ?>
+                </div>
+                <?php } ?>
+                <button type="submit" class="btn-primary">Regsitrarme</button>
+                <button type="reset" class="btn-danger">Cancelar</button>
             </form>
         </div>
     </div>
